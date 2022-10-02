0688755d47b29eae97e01e767c49d77a8614baa5
Joey Castillo <joeycastillo@utexas.edu>
2022-10-02 11:06:42 -0500

    move solar project to a d-pad based UI

--- apps/sol-train/app.c
+++ apps/sol-train/app.c
@@ -4,14 +4,24 @@
 #include "rtc.h"
 #include "adc.h"
 #include "tcc.h"
+#include "delay.h"
 #include "drivers/oso_lcd.h"
 #include <stdio.h>
 
 #define NUM_MODES 6
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 static void _display_time(rtc_date_time date_time);
 static void _display_battery_voltage(void);
 static void _display_led_value(uint8_t channel);
+
+HAL_GPIO_PIN(LEFT,   B, 8)
+HAL_GPIO_PIN(DOWN,   B, 9)
+HAL_GPIO_PIN(RIGHT,  A, 18)
+HAL_GPIO_PIN(SELECT, A, 16)
+HAL_GPIO_PIN(UP,     A, 19)
 
 void app_init(void) {
     rtc_init();
@@ -35,17 +45,35 @@ void app_setup(void) {
     rtc_date_time date_time = {0};
     rtc_enable_alarm_interrupt(date_time, ALARM_MATCH_SS);
 
-    // Button C
-    HAL_GPIO_D5_in();
-    HAL_GPIO_D5_pullup();
-    HAL_GPIO_D5_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(15, INTERRUPT_TRIGGER_FALLING);
+    // Left Button
+    HAL_GPIO_LEFT_in();
+    HAL_GPIO_LEFT_pullup();
+    HAL_GPIO_LEFT_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(8, INTERRUPT_TRIGGER_FALLING);
 
-    // Button B
-    HAL_GPIO_D6_in();
-    HAL_GPIO_D6_pullup();
-    HAL_GPIO_D6_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(4, INTERRUPT_TRIGGER_FALLING);
+    // Down Button
+    HAL_GPIO_DOWN_in();
+    HAL_GPIO_DOWN_pullup();
+    HAL_GPIO_DOWN_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(9, INTERRUPT_TRIGGER_FALLING);
+
+    // Right Button
+    HAL_GPIO_RIGHT_in();
+    HAL_GPIO_RIGHT_pullup();
+    HAL_GPIO_RIGHT_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(2, INTERRUPT_TRIGGER_FALLING);
+
+    // Select Button
+    HAL_GPIO_SELECT_in();
+    HAL_GPIO_SELECT_pullup();
+    HAL_GPIO_SELECT_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(3, INTERRUPT_TRIGGER_FALLING);
+
+    // Up Button
+    HAL_GPIO_UP_in();
+    HAL_GPIO_UP_pullup();
+    HAL_GPIO_UP_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(0, INTERRUPT_TRIGGER_FALLING);
 
     // configure the TCC peripheral
     tcc_setup(0, 2);
@@ -73,20 +101,46 @@ void app_setup(void) {
 }
 
 uint8_t mode = 4;
-uint8_t brightness[4] = {0};
+int8_t brightness[4] = {0};
 
 bool app_loop(void) {
     rtc_date_time date_time = rtc_get_date_time();
 
-    if (!HAL_GPIO_D6_read()) {
+    if (date_time.unit.hour > 19 || date_time.unit.hour < 3) {
+        if (!TCC0->CTRLA.bit.ENABLE) {
+            tcc_enable(0);
+        }
+        TCC0->CC[0].reg = brightness[0];
+        TCC0->CC[1].reg = brightness[1];
+        TCC0->CC[2].reg = brightness[2];
+        TCC0->CC[3].reg = brightness[3];
+    } else {
+        TCC0->CC[0].reg = 0;
+        TCC0->CC[1].reg = 0;
+        TCC0->CC[2].reg = 0;
+        TCC0->CC[3].reg = 0;
+        if (TCC0->CTRLA.bit.ENABLE) {
+            delay_ms(100);
+            tcc_disable(0);
+            HAL_GPIO_A3_clr();
+            HAL_GPIO_A4_clr();
+            HAL_GPIO_D0_clr();
+            HAL_GPIO_D1_clr();
+        }
+    }
+
+    if (!HAL_GPIO_LEFT_read()) {
+        if (mode == 0) mode = NUM_MODES - 1;
+        else mode = (mode - 1);
+    }
+    if (!HAL_GPIO_RIGHT_read()) {
         mode = (mode + 1) % NUM_MODES;
     }
 
-    if (!HAL_GPIO_D5_read()) {
+    if (!HAL_GPIO_UP_read()) {
         if (mode < 4) {
-            brightness[mode] += 10;
+            brightness[mode] = MIN(brightness[mode] + 10, 100);
             if (brightness[mode] > 100) brightness[mode] = 0;
-            TCC0->CC[mode].reg = brightness[mode];
         } else if (mode == 4) {
             date_time.unit.minute = (date_time.unit.minute + 1) % 60;
             if (date_time.unit.minute == 0) {
@@ -94,6 +148,27 @@ bool app_loop(void) {
             }
             date_time.unit.second = 0;
             rtc_set_date_time(date_time);
+            _display_time(date_time);
+            return false;
+        }
+    }
+
+    if (!HAL_GPIO_DOWN_read()) {
+        if (mode < 4) {
+            brightness[mode] = MAX(brightness[mode] - 10, 0);
+            if (brightness[mode] > 100) brightness[mode] = 0;
+            TCC0->CC[mode].reg = brightness[mode];
+        } else if (mode == 4) {
+            if (date_time.unit.minute == 0) {
+                date_time.unit.hour = (date_time.unit.hour + 23) % 24;
+                date_time.unit.minute = 59;
+            } else {
+                date_time.unit.minute = (date_time.unit.minute - 1) % 60;
+            }
+            date_time.unit.second = 0;
+            rtc_set_date_time(date_time);
+            _display_time(date_time);
+            return false;
         }
     }
 
@@ -106,7 +181,6 @@ bool app_loop(void) {
             break;
         case 4:
             _display_time(date_time);
-            // return true to hang out in STANDBY until an interrupt fires
             break;
         case 5:
             _display_battery_voltage();
