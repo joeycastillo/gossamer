a4a47c6826c0fe332f089a63104afe1af5eff392
joeycastillo <joeycastillo@utexas.edu>
2023-05-24 19:22:44 -0400

    add circuit playground board definition

---
+++ boards/circuitplayground_m0/pins.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "hal_gpio.h"
+
+// Analog pins
+HAL_GPIO_PIN(A0,            A,  2)
+HAL_GPIO_PIN(A1,            A,  5)
+HAL_GPIO_PIN(A2,            A,  6)
+HAL_GPIO_PIN(A3,            A,  7)
+HAL_GPIO_PIN(A4,            B,  3)
+HAL_GPIO_PIN(A5,            B,  2)
+HAL_GPIO_PIN(A6,            B,  9)
+HAL_GPIO_PIN(A7,            B,  8)
+HAL_GPIO_PIN(TEMP_SENSE,    A,  9)
+HAL_GPIO_PIN(LIGHT_SENSE,   A, 11)
+
+// Digital pins
+HAL_GPIO_PIN(LED,           A, 17)
+HAL_GPIO_PIN(BUTTON_A,      A, 28)
+HAL_GPIO_PIN(BUTTON_B,      A, 14)
+HAL_GPIO_PIN(SWITCH,        A, 15)
+HAL_GPIO_PIN(IR_TX,         A, 23)
+HAL_GPIO_PIN(IR_RX,         A, 12)
+HAL_GPIO_PIN(ACCEL_INT,     A, 27)
+
+// Neopixel
+HAL_GPIO_PIN(NEOPIXEL,      B,  23)
+
+// Flash chip
+HAL_GPIO_PIN(FLASH_SCK,     A, 21)
+HAL_GPIO_PIN(FLASH_MOSI,    A, 20)
+HAL_GPIO_PIN(FLASH_MISO,    A, 16)
+HAL_GPIO_PIN(FLASH_CS,      B, 22)
+
+// I2C
+HAL_GPIO_PIN(SDA,           A,  0)
+HAL_GPIO_PIN(SCL,           A,  1)
+#define I2C_SERCOM SERCOM1
+#define I2C_SERCOM_APBCMASK PM_APBCMASK_SERCOM1
+#define I2C_GCLK_CLKCTRL_ID GCLK_CLKCTRL_ID_SERCOM1_CORE
+
+// UART
+HAL_GPIO_PIN(RX,   B,  9)
+HAL_GPIO_PIN(TX,   B,  8)
+
+// TFT Gizmo
+HAL_GPIO_PIN(TFT_SCK,       B,  3)
+HAL_GPIO_PIN(TFT_MOSI,      B,  2)
+HAL_GPIO_PIN(TFT_BACKLIGHT, A,  7)
+HAL_GPIO_PIN(TFT_CS,        B,  9)
+HAL_GPIO_PIN(TFT_DC,        B,  8)
