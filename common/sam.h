67843a754b3d278118c15f42a0f795b886ca06a4
joeycastillo <joeycastillo@utexas.edu>
2023-05-27 09:46:52 -0400

    add more readable pin mux defines

--- common/sam.h
+++ common/sam.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#define HAL_GPIO_PMUX_EIC HAL_GPIO_PMUX_A
+#define HAL_GPIO_PMUX_ADC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_AC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_PTC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_SERCOM HAL_GPIO_PMUX_C
+#define HAL_GPIO_PMUX_SERCOM_ALT HAL_GPIO_PMUX_D
+#define HAL_GPIO_PMUX_TC_TCC HAL_GPIO_PMUX_E
+
 #if defined(__SAMD11C14A__) || defined(__ATSAMD11C14A__) || \
     defined(__SAMD11D14AM__) || defined(__ATSAMD11D14AM__) || \
     defined(__SAMD11D14AS__) || defined(__ATSAMD11D14AS__) || \
     defined(__SAMD11D14AU__) || defined(__ATSAMD11D14AU__)
 #include "samd11.h"
+#define HAL_GPIO_PMUX_DAC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_TCC_ALT HAL_GPIO_PMUX_F
+#define HAL_GPIO_PMUX_COM HAL_GPIO_PMUX_G
+#define HAL_GPIO_PMUX_GCLK HAL_GPIO_PMUX_H
 #endif
 
 #if defined(__SAMD21E15A__) || defined(__ATSAMD21E15A__) || \
@@ -22,6 +34,11 @@
     defined(__SAMD21J17A__) || defined(__ATSAMD21J17A__) || \
     defined(__SAMD21J18A__) || defined(__ATSAMD21J18A__)
 #include "samd21.h"
+#define HAL_GPIO_PMUX_DAC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_TCC_ALT HAL_GPIO_PMUX_F
+#define HAL_GPIO_PMUX_COM HAL_GPIO_PMUX_G
+#define HAL_GPIO_PMUX_GCLK HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_AC_OUTPUT HAL_GPIO_PMUX_H
 #endif
 
 #if defined(__SAML21E15B__) || defined(__ATSAML21E15B__) || \
@@ -37,6 +54,14 @@
     defined(__SAML21J18B__) || defined(__ATSAML21J18B__) || \
     defined(__SAML21J18BU__) || defined(__ATSAML21J18BU__)
 #include "saml21.h"
+#define HAL_GPIO_PMUX_DAC HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_OPAMP HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_TCC_ALT HAL_GPIO_PMUX_F
+#define HAL_GPIO_PMUX_COM HAL_GPIO_PMUX_G
+#define HAL_GPIO_PMUX_GCLK HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_SUPC HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_AC_OUTPUT HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_CCL HAL_GPIO_PMUX_I
 #endif
 
 #if defined(__SAML22G16A__) || defined(__ATSAML22G16A__) || \
@@ -49,4 +74,12 @@
     defined(__SAML22N17A__) || defined(__ATSAML22N17A__) || \
     defined(__SAML22N18A__) || defined(__ATSAML22N18A__)
 #include "saml22.h"
+#define HAL_GPIO_PMUX_SLCD HAL_GPIO_PMUX_B
+#define HAL_GPIO_PMUX_RTC_PB01 HAL_GPIO_PMUX_E
+#define HAL_GPIO_PMUX_COM HAL_GPIO_PMUX_F
+#define HAL_GPIO_PMUX_RTC HAL_GPIO_PMUX_F
+#define HAL_GPIO_PMUX_GCLK HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_SUPC HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_AC_OUTPUT HAL_GPIO_PMUX_H
+#define HAL_GPIO_PMUX_CCL HAL_GPIO_PMUX_I
 #endif
