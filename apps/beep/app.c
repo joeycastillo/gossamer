363ca5d1a6f5da60f00096a9998b6fd727061db7
joeycastillo <joeycastillo@utexas.edu>
2023-05-28 09:47:16 -0400

    big timer/counter refactor

--- apps/beep/app.c
+++ apps/beep/app.c
@@ -1,41 +1,39 @@
 #include "app.h"
 #include "delay.h"
-#include "piezo.h"
+#include "tc.h"
 
 void app_init(void) {
 }
 
 void app_setup(void) {
     HAL_GPIO_LED_out();
-    HAL_GPIO_D10_out();
-    HAL_GPIO_D10_pmuxen(HAL_GPIO_PMUX_F);
-    HAL_GPIO_D11_out();
-    HAL_GPIO_D11_pmuxen(HAL_GPIO_PMUX_F);
-    piezo_tcc_init_differential(0, 6, 7);
-    
-    // designed for use with Feather M0 and the Button/Buzzer wing.
-    // Hold button A to drive the piezo with one pin (decent loud)
-    // Hold button C to drive the piezo differentially (extra loud)
-    HAL_GPIO_D5_in();
-    HAL_GPIO_D5_pullup();
-    HAL_GPIO_D9_in();
+
+    HAL_GPIO_D9_out();
     HAL_GPIO_D9_pullup();
+
+    HAL_GPIO_D10_out();
+    HAL_GPIO_D11_out();
+    HAL_GPIO_D11_clr();
+
+    HAL_GPIO_D10_pmuxen(HAL_GPIO_PMUX_TC_TCC);
+
+    tc_setup(3, GENERIC_CLOCK_0, TC_PRESCALER_DIV8);
+    tc_set_counter_mode(3, TC_COUNTER_MODE_8BIT);
+    tc_count8_set_period(3, 250);
+    tc_set_wavegen(3, TC_WAVEGEN_NORMAL_PWM);
+    tc_count16_set_cc(3, 0, 0);
+    tc_enable(3);
 }
 
 bool app_loop(void) {
-    if (!HAL_GPIO_D5_read()) {
-        HAL_GPIO_D11_pmuxen(HAL_GPIO_PMUX_F);
-        piezo_tcc_init_differential(0, 6, 7);
-    }
     if (!HAL_GPIO_D9_read()) {
-        HAL_GPIO_D11_pmuxdis();
-        HAL_GPIO_D11_clr();
-        piezo_tcc_init(0, 6);
+        HAL_GPIO_LED_set();
+        tc_count16_set_cc(3, 0, 125);
+        delay_ms(50);
+        tc_count16_set_cc(3, 0, 0);
+        delay_ms(100);
     }
-    piezo_tcc_on(4000);
-    delay_ms(50);
-    piezo_tcc_off();
-    HAL_GPIO_LED_toggle();
-    delay_ms(500);
+    HAL_GPIO_LED_clr();
+
     return false;
 }
--- peripherals/tc.c
+++ peripherals/tc.c
@@ -25,7 +25,7 @@
 #include "pins.h"
 #include "tc.h"
 
-void tc_sync(uint8_t instance) {
+static void tc_sync(uint8_t instance) {
 #if defined(_SAMD21_) || defined(_SAMD11_)
     while (TC_Peripherals[instance - TC_First_Index].tc->COUNT16.STATUS.bit.SYNCBUSY);
 #else
@@ -56,6 +56,41 @@ bool tc_setup(uint8_t instance, generic_clock_generator_t clocksource, tc_presca
     return true;
 }
 
+void tc_set_counter_mode(uint8_t instance, tc_counter_mode_t mode) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLA.bit.MODE = mode;
+}
+
+void tc_set_run_in_standby(uint8_t instance, bool runStandby) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLA.bit.RUNSTDBY = runStandby;
+}
+
+void tc_set_wavegen(uint8_t instance, tc_wavegen_t mode) {
+#if defined(_SAMD21_) || defined(_SAMD11_)
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLA.bit.WAVEGEN = mode;
+#else
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.WAVE.bit.WAVEGEN = mode;
+#endif
+}
+
+void tc_set_channel_polarity(uint8_t instance, uint8_t channel, tc_channel_polarity_t polarity) {
+    uint8_t value = 
+#if defined(_SAMD21_) || defined(_SAMD11_)
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLC.reg;
+#else
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.DRVCTRL.reg;
+#endif
+    if (polarity == TC_CHANNEL_POLARITY_INVERTED) {
+        value |= (1 << channel);
+    } else {
+        value &= ~(1 << channel);
+    }
+#if defined(_SAMD21_) || defined(_SAMD11_)
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLC.reg = value;
+#else
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.DRVCTRL.reg = value;
+#endif
+}
+
 void tc_enable(uint8_t instance) {
 #if defined(_SAMD21_) || defined(_SAMD11_)
     PM->APBCMASK.reg |= TC_Peripherals[instance - TC_First_Index].clock_enable_mask;
@@ -64,6 +99,67 @@ void tc_enable(uint8_t instance) {
 #endif
     TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLA.bit.ENABLE = 1;
     tc_sync(instance);
+}
+
+void tc_count8_set_period(uint8_t instance, uint8_t period) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT8.PER.reg = period;
+}
+
+void tc_count8_set_cc(uint8_t instance, uint8_t channel, uint8_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT8.CC[channel].reg = value;
+}
+
+void tc_count16_set_cc(uint8_t instance, uint8_t channel, uint16_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CC[channel].reg = value;
+}
+
+void tc_count32_set_cc(uint8_t instance, uint8_t channel, uint32_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT32.CC[channel].reg = value;
+}
+
+void tc_count8_set_count(uint8_t instance, uint8_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT8.COUNT.reg = value;
+}
+
+void tc_count16_set_count(uint8_t instance, uint16_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.COUNT.reg = value;
+}
+
+void tc_count32_set_count(uint8_t instance, uint32_t value) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT32.COUNT.reg = value;
+}
+
+static void _tc_request_read_count(uint8_t instance) {
+#if defined(_SAMD21_) || defined(_SAMD11_)
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.READREQ.reg = TC_READREQ_RREQ | TC_READREQ_ADDR(0x10);
+    while (TC_Peripherals[instance - TC_First_Index].tc->COUNT16.STATUS.bit.SYNCBUSY);
+#else
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT32.CTRLBSET.bit.CMD = TC_CTRLBSET_CMD_READSYNC_Val;
+    while (TC_Peripherals[instance - TC_First_Index].tc->COUNT16.SYNCBUSY.reg);
+#endif
+}
+
+uint8_t tc_count8_get_count(uint8_t instance) {
+    _tc_request_read_count(instance);
+    return TC_Peripherals[instance - TC_First_Index].tc->COUNT8.COUNT.reg;    
+}
+
+uint16_t tc_count16_get_count(uint8_t instance) {
+    _tc_request_read_count(instance);
+    return TC_Peripherals[instance - TC_First_Index].tc->COUNT16.COUNT.reg;
+}
+
+uint32_t tc_count32_get_count(uint8_t instance) {
+    _tc_request_read_count(instance);
+    return TC_Peripherals[instance - TC_First_Index].tc->COUNT32.COUNT.reg;
+}
+
+void tc_stop(uint8_t instance) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLBSET.bit.CMD = TC_CTRLBSET_CMD_STOP_Val;
+}
+
+void tc_retrigger(uint8_t instance) {
+    TC_Peripherals[instance - TC_First_Index].tc->COUNT16.CTRLBSET.bit.CMD = TC_CTRLBSET_CMD_RETRIGGER_Val;
 }
 
 void tc_disable(uint8_t instance) {
