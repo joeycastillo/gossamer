1cbed0ce180af481b54387ac1f7eb9639cd72bcd
Jose Castillo <jc3292@cornell.edu>
2023-06-01 10:26:48 -0400

    read a bit of temperature data

--- apps/spi/app.c
+++ apps/spi/app.c
@@ -8,7 +8,7 @@
 // simple test with a BME280 breakout board.
 // wire to SPI pins on a Feather M0, CS on pin 1 (TX), and an
 // Oddly Specific LCD on the I2C pins.
-// Should display the chip ID (58) on the LCD.
+// Should display a frequently changing byte of raw temperature data.
 
 void app_init(void) {
 }
@@ -28,7 +28,16 @@ void app_setup(void) {
     HAL_GPIO_MOSI_pmuxen(HAL_GPIO_PMUX_SERCOM_ALT);
     HAL_GPIO_MISO_pmuxen(HAL_GPIO_PMUX_SERCOM_ALT);
     spi_init(SPI_MODE_0, 8000000);
-    spi_enable(SPI_MODE_0, 8000000);
+    spi_enable();
+    HAL_GPIO_D1_clr();
+    spi_transfer(0xe0);
+    spi_transfer(0xb6);
+    HAL_GPIO_D1_set();
+    HAL_GPIO_D1_clr();
+    spi_transfer(0x74);
+    spi_transfer(0b00100111);
+    HAL_GPIO_D1_set();
+    delay_ms(10);
 
     i2c_init();
     i2c_enable();
@@ -42,13 +51,14 @@ bool app_loop(void) {
 
     oso_lcd_set_indicator(OSO_LCD_INDICATOR_DATA);
     HAL_GPIO_D1_clr();
-    spi_transfer(0xD0);
+    spi_transfer(0xfa);
+    data = spi_transfer(0x00);
     data = spi_transfer(0x00);
     HAL_GPIO_D1_set();
     oso_lcd_clear_indicator(OSO_LCD_INDICATOR_DATA);
 
-    sprintf(buf, "1d: %02x", data);
+    sprintf(buf, "td: %02x", data);
     oso_lcd_print(buf);
 
-    return true;
+    return false;
 }
