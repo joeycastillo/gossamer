55910d678cedcf0c824291da6e1275ea57a46cc8
joeycastillo <joeycastillo@utexas.edu>
2023-06-07 23:34:24 -0400

    add midi / msc changes to tinyusb config

--- common/tusb_config.h
+++ common/tusb_config.h
@@ -109,6 +109,13 @@
 // CDC Endpoint transfer buffer size, more is faster
 #define CFG_TUD_CDC_EP_BUFSIZE (64)
 
+// MIDI FIFO size of TX and RX
+#define CFG_TUD_MIDI_RX_BUFSIZE (64)
+#define CFG_TUD_MIDI_TX_BUFSIZE (64)
+
+// MSC Buffer size of Device Mass storage
+#define CFG_TUD_MSC_EP_BUFSIZE   (512)
+
 #ifdef __cplusplus
  }
 #endif
