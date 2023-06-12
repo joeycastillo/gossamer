592aa725ee95ed4250e9ef14cbf7ad2790b2c4b0
joeycastillo <joeycastillo@users.noreply.github.com>
2023-06-12 19:40:30 +0000

    deploy: 0f15189e63870e2fdce1fc5ba4133815bc8d9e64

--- search/files_9.js
+++ search/files_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['sh1107_2eh_0',['sh1107.h',['../sh1107_8h.html',1,'']]],
-  ['spi_2eh_1',['spi.h',['../spi_8h.html',1,'']]],
-  ['system_2eh_2',['system.h',['../system_8h.html',1,'']]]
+  ['sercom_2eh_0',['sercom.h',['../sercom_8h.html',1,'']]],
+  ['sh1107_2eh_1',['sh1107.h',['../sh1107_8h.html',1,'']]],
+  ['spi_2eh_2',['spi.h',['../spi_8h.html',1,'']]],
+  ['system_2eh_3',['system.h',['../system_8h.html',1,'']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,7 +1,11 @@
 var searchData=
 [
   ['i2c_5fdisable_0',['i2c_disable',['../i2c_8h.html#abe70f8fe4c72329b89ec526c749f50d7',1,'i2c.h']]],
-  ['i2c_5fenable_1',['i2c_enable',['../i2c_8h.html#a5a1923e2271df541e858cb4e05d7b895',1,'i2c.h']]],
-  ['i2c_5finit_2',['i2c_init',['../i2c_8h.html#a5730d9445429351b9f750084c5cb5aae',1,'i2c.h']]],
-  ['i2c_5fwrite_3',['i2c_write',['../i2c_8h.html#a9ecbbaf0c77591bc4a80e6ecd943e64c',1,'i2c.h']]]
+  ['i2c_5fdisable_5fcustom_1',['i2c_disable_custom',['../i2c_8h.html#a8a5a5fb6391172e4af6dc8628b38b8c1',1,'i2c.c']]],
+  ['i2c_5fenable_2',['i2c_enable',['../i2c_8h.html#a5a1923e2271df541e858cb4e05d7b895',1,'i2c.h']]],
+  ['i2c_5fenable_5fcustom_3',['i2c_enable_custom',['../i2c_8h.html#a9d1cd408436574ff30998079b76db7f6',1,'i2c.c']]],
+  ['i2c_5finit_4',['i2c_init',['../i2c_8h.html#a5730d9445429351b9f750084c5cb5aae',1,'i2c.h']]],
+  ['i2c_5finit_5fcustom_5',['i2c_init_custom',['../i2c_8h.html#a4070841b773d898e8a7fb5fb27dab633',1,'i2c.c']]],
+  ['i2c_5fwrite_6',['i2c_write',['../i2c_8h.html#a9ecbbaf0c77591bc4a80e6ecd943e64c',1,'i2c.h']]],
+  ['i2c_5fwrite_5fcustom_7',['i2c_write_custom',['../i2c_8h.html#a680126a406db022e2566e01394237de7',1,'i2c.c']]]
 ];
