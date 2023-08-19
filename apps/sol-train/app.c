de223e2309f58f5c09cc60114d35689636133bf1
joeycastillo <joeycastillo@utexas.edu>
2023-08-19 08:37:54 -0400

    WIP on subway serpent

--- apps/sol-train/app.c
+++ apps/sol-train/app.c
@@ -17,72 +17,80 @@ static void _display_time(rtc_date_time date_time);
 static void _display_battery_voltage(void);
 static void _display_led_value(uint8_t channel);
 
-HAL_GPIO_PIN(LEFT,   B, 8)
-HAL_GPIO_PIN(DOWN,   B, 9)
-HAL_GPIO_PIN(RIGHT,  A, 18)
-HAL_GPIO_PIN(SELECT, A, 16)
-HAL_GPIO_PIN(UP,     A, 19)
-
 void app_init(void) {
+    // Confugure brown-out detector to put us into
+    // BACKUP mode when main power fails
+    SUPC->BOD33.bit.ACTION = SUPC_BOD33_ACTION_BKUP_Val;
+    // Enable sampling in BACKUP mode
+    SUPC->BOD33.bit.PSEL = SUPC_BOD33_PSEL_DIV256_Val;
+    // Run brown-out detector in backup mode, so that
+    // we can detect power restoration
+    SUPC->BOD33.bit.RUNBKUP = 1;
+    // Enable brown-out detector
+    SUPC->BOD33.bit.ENABLE = 1;
+
+    // Setting WAKEEN in will ensure that we wake up
+    // when power is restored.
+    SUPC->BBPS.bit.WAKEEN = 1;
+
     rtc_init();
     rtc_enable();
 }
 
 void app_setup(void) {
+    HAL_GPIO_LED_out();
+
     // set up the external interrupt controller
     eic_init();
     eic_enable();
 
+    // set up (but do not enable) the ADC
+    adc_init();
+    // we have a 200K voltage divider on the battery input, so we
+    // need to increase the sampling time to 6 ADC clock cycles
+    ADC->SAMPCTRL.bit.SAMPLEN = 6;
+
     // set up the I²C peripheral...
     HAL_GPIO_SDA_out();
     HAL_GPIO_SCL_out();
-    HAL_GPIO_SDA_pmuxen(HAL_GPIO_PMUX_D);
-    HAL_GPIO_SCL_pmuxen(HAL_GPIO_PMUX_D);
+    HAL_GPIO_SDA_pmuxen(HAL_GPIO_PMUX_C);
+    HAL_GPIO_SCL_pmuxen(HAL_GPIO_PMUX_C);
     i2c_init();
     i2c_enable();
     // ...and the Oddly Specific LCD
     oso_lcd_begin();
-    oso_lcd_fill(0);
+    oso_lcd_fill(1);
 
     // fire RTC alarm every minute
     rtc_date_time date_time = {0};
     rtc_enable_alarm_interrupt(date_time, ALARM_MATCH_SS);
 
-    // Left Button
-    HAL_GPIO_LEFT_in();
-    HAL_GPIO_LEFT_pullup();
-    HAL_GPIO_LEFT_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(8, INTERRUPT_TRIGGER_FALLING);
-
     // Down Button
-    HAL_GPIO_DOWN_in();
-    HAL_GPIO_DOWN_pullup();
-    HAL_GPIO_DOWN_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(9, INTERRUPT_TRIGGER_FALLING);
-
-    // Right Button
-    HAL_GPIO_RIGHT_in();
-    HAL_GPIO_RIGHT_pullup();
-    HAL_GPIO_RIGHT_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(2, INTERRUPT_TRIGGER_FALLING);
-
-    // Select Button
-    HAL_GPIO_SELECT_in();
-    HAL_GPIO_SELECT_pullup();
-    HAL_GPIO_SELECT_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(3, INTERRUPT_TRIGGER_FALLING);
+    HAL_GPIO_D5_in();
+    HAL_GPIO_D5_pullup();
+    HAL_GPIO_D5_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(15, INTERRUPT_TRIGGER_FALLING);
 
     // Up Button
-    HAL_GPIO_UP_in();
-    HAL_GPIO_UP_pullup();
-    HAL_GPIO_UP_pmuxen(HAL_GPIO_PMUX_A);
-    eic_configure_channel(0, INTERRUPT_TRIGGER_FALLING);
+    HAL_GPIO_D6_in();
+    HAL_GPIO_D6_pullup();
+    HAL_GPIO_D6_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(4, INTERRUPT_TRIGGER_FALLING);
+
+    // Mode Button
+    HAL_GPIO_D9_in();
+    HAL_GPIO_D9_pullup();
+    HAL_GPIO_D9_pmuxen(HAL_GPIO_PMUX_A);
+    eic_configure_channel(7, INTERRUPT_TRIGGER_FALLING);
 
     // configure the TCC peripheral
     tcc_init(0, GENERIC_CLOCK_2, TCC_PRESCALER_DIV1);
+
     tcc_set_wavegen(0, TCC_WAVEGEN_NORMAL_PWM);
     tcc_set_period(0, 100);
     tcc_set_run_in_standby(0, true);
+
+    tcc_enable(0);
 
     // TCC0 waveform output 0
     HAL_GPIO_A3_out();
@@ -109,15 +117,11 @@ int8_t brightness[4] = {0};
 bool app_loop(void) {
     rtc_date_time date_time = rtc_get_date_time();
 
-    if (!HAL_GPIO_LEFT_read()) {
-        if (mode == 0) mode = NUM_MODES - 1;
-        else mode = (mode - 1);
-    }
-    if (!HAL_GPIO_RIGHT_read()) {
+    if (!HAL_GPIO_D9_read()) {
         mode = (mode + 1) % NUM_MODES;
     }
 
-    if (!HAL_GPIO_UP_read()) {
+    if (!HAL_GPIO_D6_read()) {
         if (mode < 4) {
             brightness[mode] = MIN(brightness[mode] + 10, 100);
             if (brightness[mode] > 100) brightness[mode] = 0;
@@ -133,7 +137,7 @@ bool app_loop(void) {
         }
     }
 
-    if (!HAL_GPIO_DOWN_read()) {
+    if (!HAL_GPIO_D5_read()) {
         if (mode < 4) {
             brightness[mode] = MAX(brightness[mode] - 10, 0);
             if (brightness[mode] > 100) brightness[mode] = 0;
