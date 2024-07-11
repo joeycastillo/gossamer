a84d73c964b96dba57e9c0cfea0cc6f711eb6968
joeycastillo <joeycastillo@users.noreply.github.com>
2024-07-11 20:12:42 +0000

    deploy: 62df07a0c96017d73b01c3e0ff44c312de2654c4

--- search/all_1.js
+++ search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_5fenter_5fstandby_5fmode_0',['_enter_standby_mode',['../system_8h.html#a436af75ad5caf82faa3497787abf4321',1,'system.h']]]
+  ['_5fenable_5f48mhz_5fgclk1_0',['_enable_48mhz_gclk1',['../system_8h.html#af6d6cb9ca08e26e456d9ed1d425453e8',1,'system.h']]],
+  ['_5fenter_5fstandby_5fmode_1',['_enter_standby_mode',['../system_8h.html#a436af75ad5caf82faa3497787abf4321',1,'system.h']]]
 ];
