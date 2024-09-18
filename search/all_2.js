7626f97ab3d30e970be40f07172a789bf947f0d3
joeycastillo <joeycastillo@users.noreply.github.com>
2024-09-18 18:21:16 +0000

    deploy: 9296c4055c35049fc29cb51c3bc1f7f8a4cf14df

--- search/all_2.js
+++ search/all_2.js
@@ -6,7 +6,7 @@ var searchData=
   ['adc_2eh_3',['adc.h',['../adc_8h.html',1,'']]],
   ['adc_5fdisable_4',['adc_disable',['../adc_8h.html#a3b4b977d0665b5346849f3b892ac5106',1,'adc.c']]],
   ['adc_5fenable_5',['adc_enable',['../adc_8h.html#a3afdea3b7c9b91013c270f1a1a669782',1,'adc.c']]],
-  ['adc_5fget_5fanalog_5fvalue_6',['adc_get_analog_value',['../adc_8h.html#a2c8f49eff87c1a55de3b11afe59cae37',1,'adc.c']]],
+  ['adc_5fget_5fanalog_5fvalue_6',['adc_get_analog_value',['../adc_8h.html#a96ce001b81ec65219abcb7e4a583f55c',1,'adc.c']]],
   ['adc_5fget_5fanalog_5fvalue_5ffor_5fchannel_7',['adc_get_analog_value_for_channel',['../adc_8h.html#a200744370e2a8505c3dead71aff1c2dc',1,'adc.c']]],
   ['adc_5finit_8',['adc_init',['../adc_8h.html#a2b815e6730e8723a6d1d06d9ef8f31c0',1,'adc.c']]],
   ['adc_5fset_5fsampling_5flength_9',['adc_set_sampling_length',['../adc_8h.html#a1b33d0d9bd2841354f016f8c64be3e15',1,'adc.c']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['adc_5fdisable_0',['adc_disable',['../adc_8h.html#a3b4b977d0665b5346849f3b892ac5106',1,'adc.c']]],
   ['adc_5fenable_1',['adc_enable',['../adc_8h.html#a3afdea3b7c9b91013c270f1a1a669782',1,'adc.c']]],
-  ['adc_5fget_5fanalog_5fvalue_2',['adc_get_analog_value',['../adc_8h.html#a2c8f49eff87c1a55de3b11afe59cae37',1,'adc.c']]],
+  ['adc_5fget_5fanalog_5fvalue_2',['adc_get_analog_value',['../adc_8h.html#a96ce001b81ec65219abcb7e4a583f55c',1,'adc.c']]],
   ['adc_5fget_5fanalog_5fvalue_5ffor_5fchannel_3',['adc_get_analog_value_for_channel',['../adc_8h.html#a200744370e2a8505c3dead71aff1c2dc',1,'adc.c']]],
   ['adc_5finit_4',['adc_init',['../adc_8h.html#a2b815e6730e8723a6d1d06d9ef8f31c0',1,'adc.c']]],
   ['adc_5fset_5fsampling_5flength_5',['adc_set_sampling_length',['../adc_8h.html#a1b33d0d9bd2841354f016f8c64be3e15',1,'adc.c']]],
