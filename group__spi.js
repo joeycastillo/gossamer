0f32c02485368c7d0a1428aca28b37acac03dac4
joeycastillo <joeycastillo@users.noreply.github.com>
2024-10-15 01:09:18 +0000

    deploy: 30cccd0331048e971fdbce4d8ea0aebf7728350b

--- group__spi.js
+++ group__spi.js
@@ -4,8 +4,8 @@ var group__spi =
     [ "spi_disable_instance", "group__spi.html#ga3856b233dc7584708ab420d41c4816fe", null ],
     [ "spi_enable", "group__spi.html#ga72cf41d2f19954fefd27f681d6b39532", null ],
     [ "spi_enable_instance", "group__spi.html#ga8f1915874847b4564ab22039f2b8762f", null ],
-    [ "spi_init", "group__spi.html#ga48ef10ade34f68b96d36dee03ebc4415", null ],
-    [ "spi_init_instance", "group__spi.html#gaee8eaa5f597d774c4fc4a6f7d6d08305", null ],
+    [ "spi_init", "group__spi.html#gaa2b2013ee668e73a427b2a99da1ca47a", null ],
+    [ "spi_init_instance", "group__spi.html#ga236f4397be3ed06d5158e8dbfe387a29", null ],
     [ "spi_transfer", "group__spi.html#ga50084189a01c481aebe4e4d1e1968910", null ],
     [ "spi_transfer_instance", "group__spi.html#ga03d51191e1fb189fcddf1087999d58a2", null ]
 ];
