2f6c9eae4bab16d2b7d7d9fa17d997124a4405ac
Joey Castillo <joeycastillo@utexas.edu>
2025-05-17 18:43:24 -0400

    update irda_tx for new sensor watch upload feature

--- examples/irda_tx/app.c
+++ examples/irda_tx/app.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "app.h"
 #include "uart.h"
 #include "delay.h"
+
+const char filedata[] = "JBSWY3DPEHPK3PXPJBSWY3DPEHPK3PXPJBSWY3DPEHPK3PXPJBSWY3DPEE01234"; // this is test scecret, it is not real
+const char filename[] = "test.otp";
+char *upload_buffer;
+size_t buflen = 16 + sizeof(filedata) + 2;
 
 void app_init(void) {
 }
@@ -14,18 +20,36 @@ void app_setup(void) {
     uart_init_instance(4, UART_TXPO_0, UART_RXPO_NONE, 900);
     uart_set_irda_mode_instance(4, true);
     uart_enable_instance(4);
+
+    uint16_t checksum = 0;
+
+    upload_buffer = malloc(buflen);
+    memset(upload_buffer, 0, buflen);
+
+    // First 16 bytes are our header.
+    // Bytes 0-1: the size of the data to come after the header.
+    ((uint16_t *) upload_buffer)[0] = sizeof(filedata);
+    // bytes 2-13: filename with extension
+    memcpy(upload_buffer + 2, filename, sizeof(filename));
+    
+    // compute a checksum of the header data
+    for (size_t i = 0; i < 14; i++) {
+        checksum += upload_buffer[i];
+    }
+    // bytes 14-15: the header checksum
+    ((uint16_t *) upload_buffer)[7] = checksum;
+    checksum = 0;
+
+    memcpy(upload_buffer + 16, filedata, sizeof(filedata));
+    for (size_t i = 16; i < buflen - 2; i++) {
+        checksum += upload_buffer[i];
+    }
+    memcpy(upload_buffer + buflen - 2, &checksum, 2);
 }
 
 bool app_loop(void) {
-    char hello[20] = ">1TEST>Hello World*";
-    uint8_t checksum = 0;
-    for (size_t i = 0; i < strlen(hello) - 1; i++) {
-        checksum += hello[i];
-    }
-    hello[18] = checksum;
-    hello[19] = 0;
     HAL_GPIO_LED_toggle();
-    uart_write_instance(4, hello, strlen(hello));
+    uart_write_instance(4, upload_buffer, buflen);
     delay_ms(50);
     HAL_GPIO_LED_toggle();
     delay_ms(1100);
